66eea73960c27be9cc055a6a8afbe5490ca772a9
unknown <martinaoyhenard@gmail.com>
2019-05-19 15:21:35 +0200

    added link

---
+++ JavaScript/1.js
@@ -0,0 +1,17 @@
+ass = require("assert")
+const assert = ass.deepEqual
+const p = console.log
+const print = p
+print("==============")
+
+
+//f = (x) => typeof x 
+
+
+
+//assert(f(3), "number")
+//assert(f(""), "string")
+
+
+print("ok")
+print("==============")
